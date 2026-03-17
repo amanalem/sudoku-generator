99f1b0c94e34ccdeb5fcceb950bed8004d2d8a4f
amanalem <alem.aman@gmail.com>
2026-03-17 15:28:47 -0400

    Created find_hidden_singles() function which combines find row singles, find column singles, find box singles, and loops them until no more hidden singles are present

--- server/generate_sudoku_handler.py
+++ server/generate_sudoku_handler.py
@@ -1,4 +1,4 @@
-from random import randint, randrange, shuffle
+import random
 
 def make_cell():
     cell = {
@@ -28,7 +28,7 @@ def propogate(board, row, column, answer):
         if answer in cell["options"]:
             cell["options"].remove(answer)
 
-def find_singles(board):
+def find_naked_singles(board):
     while True:
         option_found = False
         for row in range(9):
@@ -39,10 +39,87 @@ def find_singles(board):
                     board[row][column]["answer"] = answer
                     propogate(board, row, column, answer)
                     option_found = True
-        if option_found == False:
+        if not option_found:
             break
 
+def find_row_singles(board):
+    for row in range(9):
+        cells = get_row(board, row)
+        for number in range(1, 10):
+            possible_cells = [i for i, cell in enumerate(cells) if number in cell["options"] and cell["answer"] is None]
+            if len(possible_cells) == 1:
+                column = possible_cells[0]
+                board[row][column]["answer"] = number
+                board[row][column]["options"] = [number]
+                propogate(board, row, column, number)
 
+def find_column_singles(board):
+    for column in range(9):
+        cells = get_column(board, column)
+        for number in range(1, 10):
+            possible_cells = [i for i, cell in enumerate(cells) if number in cell["options"] and cell["answer"] is None]
+            if len(possible_cells) == 1:
+                row = possible_cells[0]
+                board[row][column]["answer"] = number
+                board[row][column]["options"] = [number]
+                propogate(board, row, column, number)
+
+def find_box_singles(board):
+    for box_row in range(3):
+        for box_column in range(3):
+            cells = get_box(board, box_row * 3, box_column * 3)
+            for number in range(1, 10):
+                possible_cells = [(i, cell) for i, cell in enumerate(cells) if number in cell["options"] and cell["answer"] is None]
+                if len(possible_cells) == 1:
+                    index = possible_cells[0][0]
+                    row = box_row * 3 + index // 3
+                    column = box_column * 3 + index % 3
+                    board[row][column]["answer"] = number
+                    board[row][column]["options"] = [number]
+                    propogate(board, row, column, number)
+
+
+def find_hidden_singles(board):
+    while True:
+        repeat = False
+        # First we look for hidden row singles
+        for row in range(9):
+            cells = get_row(board, row)
+            for number in range(1, 10):
+                possible_cells = [i for i, cell in enumerate(cells) if number in cell["options"] and cell["answer"] is None]
+                if len(possible_cells) == 1:
+                    column = possible_cells[0]
+                    board[row][column]["answer"] = number
+                    board[row][column]["options"] = [number]
+                    propogate(board, row, column, number)
+                    repeat = True
+        # Second we look for hidden column singles
+        for column in range(9):
+            cells = get_column(board, column)
+            for number in range(1, 10):
+                possible_cells = [i for i, cell in enumerate(cells) if number in cell["options"] and cell["answer"] is None]
+                if len(possible_cells) == 1:
+                    row = possible_cells[0]
+                    board[row][column]["answer"] = number
+                    board[row][column]["options"] = [number]
+                    propogate(board, row, column, number)
+                    repeat = True
+        # Third we look for hidden box singles
+        for box_row in range(3):
+            for box_column in range(3):
+                cells = get_box(board, box_row * 3, box_column * 3)
+                for number in range(1, 10):
+                    possible_cells = [(i, cell) for i, cell in enumerate(cells) if number in cell["options"] and cell["answer"] is None]
+                    if len(possible_cells) == 1:
+                        index = possible_cells[0][0]
+                        row = box_row * 3 + index // 3
+                        column = box_column * 3 + index % 3
+                        board[row][column]["answer"] = number
+                        board[row][column]["options"] = [number]
+                        propogate(board, row, column, number)
+                        repeat = True
+        if not repeat:
+            break
 
 def find_best_cell(board):
     best = None
