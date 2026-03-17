58eb9120e4dc3639fa0b04689bba20fbde5c9cbe
amanalem <alem.aman@gmail.com>
2026-03-17 11:00:52 -0400

    The following functions all defined: make_cell, get_row, get_column, get_box, propogate, find_only_options, find_best_cell

--- server/generate_sudoku_handler.py
+++ server/generate_sudoku_handler.py
@@ -1,281 +1,61 @@
-from random import randint, randrange
+from random import randint, randrange, shuffle
 
-def generate_sudoku():
-
-    r1c1 = ""
-    r1c2 = ""
-    r1c3 = ""
-    r1c4 = ""
-    r1c5 = ""
-    r1c6 = ""
-    r1c7 = ""
-    r1c8 = ""
-    r1c9 = ""
-    r2c1 = ""
-    r2c2 = ""
-    r2c3 = ""
-    r2c4 = ""
-    r2c5 = ""
-    r2c6 = ""
-    r2c7 = ""
-    r2c8 = ""
-    r2c9 = ""
-    r3c1 = ""
-    r3c2 = ""
-    r3c3 = ""
-    r3c4 = ""
-    r3c5 = ""
-    r3c6 = ""
-    r3c7 = ""
-    r3c8 = ""
-    r3c9 = ""
-    r4c1 = ""
-    r4c2 = ""
-    r4c3 = ""
-    r4c4 = ""
-    r4c5 = ""
-    r4c6 = ""
-    r4c7 = ""
-    r4c8 = ""
-    r4c9 = ""
-    r5c1 = ""
-    r5c2 = ""
-    r5c3 = ""
-    r5c4 = ""
-    r5c5 = ""
-    r5c6 = ""
-    r5c7 = ""
-    r5c8 = ""
-    r5c9 = ""
-    r6c1 = ""
-    r6c2 = ""
-    r6c3 = ""
-    r6c4 = ""
-    r6c5 = ""
-    r6c6 = ""
-    r6c7 = ""
-    r6c8 = ""
-    r6c9 = ""
-    r7c1 = ""
-    r7c2 = ""
-    r7c3 = ""
-    r7c4 = ""
-    r7c5 = ""
-    r7c6 = ""
-    r7c7 = ""
-    r7c8 = ""
-    r7c9 = ""
-    r8c1 = ""
-    r8c2 = ""
-    r8c3 = ""
-    r8c4 = ""
-    r8c5 = ""
-    r8c6 = ""
-    r8c7 = ""
-    r8c8 = ""
-    r8c9 = ""
-    r9c1 = ""
-    r9c2 = ""
-    r9c3 = ""
-    r9c4 = ""
-    r9c5 = ""
-    r9c6 = ""
-    r9c7 = ""
-    r9c8 = ""
-    r9c9 = ""
-        
-    
-
-    sudoku = {
-        "rows": [
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8, r1c9]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r2c1, r2c2, r2c3, r2c4, r2c5, r2c6, r2c7, r2c8, r2c9]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r3c1, r3c2, r3c3, r3c4, r3c5, r3c6, r3c7, r3c8, r3c9]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r4c1, r4c2, r4c3, r4c4, r4c5, r4c6, r4c7, r4c8, r4c9]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r5c1, r5c2, r5c3, r5c4, r5c5, r5c6, r5c7, r5c8, r5c9]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r6c1, r6c2, r6c3, r6c4, r6c5, r6c6, r6c7, r6c8, r6c9]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r7c1, r7c2, r7c3, r7c4, r7c5, r7c6, r7c7, r7c8, r7c9]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r8c1, r8c2, r8c3, r8c4, r8c5, r8c6, r8c7, r8c8, r8c9]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r9c1, r9c2, r9c3, r9c4, r9c5, r9c6, r9c7, r9c8, r9c9]
-            },
-            
-        ],
-        "cols":[
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c1, r2c1, r3c1, r4c1, r5c1, r6c1, r7c1, r8c1, r9c1]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c2, r2c2, r3c2, r4c2, r5c2, r6c2, r7c2, r8c2, r9c2]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c3, r2c3, r3c3, r4c3, r5c3, r6c3, r7c3, r8c3, r9c3]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c4, r2c4, r3c4, r4c4, r5c4, r6c4, r7c4, r8c4, r9c4]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c5, r2c5, r3c5, r4c5, r5c5, r6c5, r7c5, r8c5, r9c5]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c6, r2c6, r3c6, r4c6, r5c6, r6c6, r7c6, r8c6, r9c6]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c7, r2c7, r3c7, r4c7, r5c7, r6c7, r7c7, r8c7, r9c7]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c8, r2c8, r3c8, r4c8, r5c8, r6c8, r7c8, r8c8, r9c8]
-            },
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c9, r2c9, r3c9, r4c9, r5c9, r6c9, r7c9, r8c9, r9c9]
-            },
-        ],
-        "boxes":[
-            {
-                "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                "cells": [r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3]
-            }
-        ]
+def make_cell():
+    cell = {
+        "options": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        "answer": None
     }
-   
-    rows = [
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "vals": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        }
-    ]
+    cell["numOfOptions"] = lambda: len(cell['options'])
+    return cell
 
-    cols = [
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        },
-        {
-            "ops": ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "val": ""
-        }
-    ]
+board = [[make_cell() for _ in range(9)] for _ in range(9)]
 
-   
-    col1 = []
-    col2 = []
-    col3 = []
-    colsx = {
-        "1": col1,
-        "2": col2,
-        "3": col3,
-        "4": col4,
-        "5": col5,
-        "6": col6,
-        "7": col7,
-        "8": col8,
-        "9": col9
-    }
+def get_row(board, row):
+    return board[row]
 
-    # Functions 
-    # list the possible number of answers remaining in each cell.
-    # cell with the fewest number of options of possible answers, and pick a value at random from its options
-    # If there is a cell in a row that has one possible answer that none of the other cells have: 
-    #   cell = that answer; 
-    #   remove answer from box possibilities; 
-    #   remove answer from column possibilities;
+def get_column(board, column):
+    return [board[row][column] for row in range(9)]
 
-    def remove_option(row, col, box, value):
+def get_box(board, row, column):
+    box_row = (row // 3) * 3
+    box_column = (column // 3) * 3
+    return [board[box_row + r][box_column + c] for r in range(3) for c in range(3)]
+
+def propogate(board, row, column, answer):
+    related_cells = get_row(board, row) + get_column(board, column) + get_box(board, row, column)
+
+    for cell in related_cells:
+        if answer in cell["options"]:
+            cell["options"].remove(answer)
+
+def find_only_options(board):
+    while True:
+        option_found = False
+        for row in range(9):
+            for column in range(9):
+                cell = board[row][column]
+                if cell["numOfOptions"] == 1 and cell["answer"] == None:
+                    answer = cell["options"][0]
+                    board[row][column]["answer"] = answer
+                    propogate(board, row, column, answer)
+                    option_found = True
+        if option_found == False:
+            break
 
 
+
+
+def find_best_cell(board):
+    best = None
+    best_count = 10
+
+    for row in range(9):
+        for column in range(9):
+            cell = board[row][column]
+            if cell["answer"] == "" and cell["numOfOptions"] < best_count:
+                best = (row, column)
+                best_count = cell["numOfOptions"]
+
+    return best
 
 
