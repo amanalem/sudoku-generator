a2a109b8950f9b1768ceca9aabb810a08a25bd64
amanalem <alem.aman@gmail.com>
2026-03-17 11:26:07 -0400

    board variable now established in app.py file rather than in the generate_sudoku_handler.py file

--- server/app.py
+++ server/app.py
@@ -9,7 +9,9 @@ CORS(app) #allows React App to talk to Flask
 def get_puzzle():
     #Sudoku Generation Logic
 
-    puzzle = generate_sudoku()
+    board = [[make_cell() for _ in range(9)] for _ in range(9)]
+
+    puzzle = 
     return jsonify({"puzzle": puzzle})
 
 if __name__ == '__main__':
