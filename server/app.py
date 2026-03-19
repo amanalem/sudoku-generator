35fda49e4bf4b8c759f8a4e504ea51f9971ca433
amanalem <alem.aman@gmail.com>
2026-03-19 09:54:31 -0400

    generate sudoku added to app.py

--- server/app.py
+++ server/app.py
@@ -7,11 +7,8 @@ CORS(app) #allows React App to talk to Flask
 
 @app.route('/api/puzzle', methods=['GET'])
 def get_puzzle():
-    #Sudoku Generation Logic
 
-    board = [[make_cell() for _ in range(9)] for _ in range(9)]
-
-    puzzle = [[board[row][column]["answer"] for column in range(9)] for row in range(9)]
+    puzzle = generate_sudoku()
     return jsonify({"puzzle": puzzle})
 
 if __name__ == '__main__':
