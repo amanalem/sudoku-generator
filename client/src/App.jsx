77b261461c6c856e92f88dfa663376e1a02a2ee8
amanalem <alem.aman@gmail.com>
2026-03-23 09:48:16 -0400

    new buttons for difficulty levels. Front end sends difficulty and puzzle is generated.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,17 @@ import SudokuGrid from "./components/SudokuGrid";
 
 function App() {
   const [puzzle, setPuzzle] = useState(null);
+  const [solution, setSolution] = useState(null);
   const [loading, setLoading] = useState(false);
   const [emptyGrid, setEmptyGrid] = useState(true);
 
-  const fetchPuzzle = async () => {
+  const fetchPuzzle = async (difficulty) => {
     setLoading(true);
     try {
-      const res = await axios.get("http://localhost:8080/api/puzzle");
+      const res = await axios.get('http://localhost:8080/api/puzzle?difficulty=${difficulty}');
       setEmptyGrid(null);
       setPuzzle(res.data.puzzle);
+      setSolution(res.data.solution)
     } catch (error) {
       console.error("Error fetching puzzle:", error);
     }
@@ -32,14 +34,17 @@ function App() {
       <h1>Sudoku Generator</h1>
       <button
         
-        onClick={fetchPuzzle}
+        onClick={() => fetchPuzzle('easy')}
         disabled={loading}
       >
-        {loading ? "Generate Solution" : "Generate Solution"}
+        Easy
       </button>
+      <button onClick={() => fetchPuzzle('medium')}>Medium</button>
+      <button onClick={() => fetchPuzzle('hard')}>Hard</button>
+      <button onClick={() => fetchPuzzle('expert')}>Expert</button>
       {emptyGrid && <EmptyGrid/>}
       {puzzle && <SudokuGrid puzzle={puzzle} />}
-      <button onClick={fetchEmptyGrid}>{"Clear Grid"}</button>
+      <button onClick={fetchEmptyGrid}>Clear Grid</button>
     </div>
   );
 }
