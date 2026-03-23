5c9d83e0757fa5319504949b7bd2cbfcd51a3527
amanalem <alem.aman@gmail.com>
2026-03-23 10:01:39 -0400

    removed empty_grid_handler import from app.py as module no longer exists

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from generate_sudoku_handler import generate_sudoku, create_puzzle
-from empty_grid_handler import get_empty_grid
 
 app = Flask(__name__)
 CORS(app) #allows React App to talk to Flask
