a1a2eae710062949c57543fc10beb504f2b7a535
amanalem <alem.aman@gmail.com>
2026-03-23 09:29:51 -0400

    app.route changed in app.py to reflect ability to create puzzles and to be able to pass difficulty from front end to backend

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from generate_sudoku_handler import generate_sudoku
+from generate_sudoku_handler import generate_sudoku create_puzzle
 from empty_grid_handler import get_empty_grid
 
 app = Flask(__name__)
@@ -8,10 +8,10 @@ CORS(app) #allows React App to talk to Flask
 
 @app.route('/api/puzzle', methods=['GET'])
 def get_puzzle():
-
-    puzzle = generate_sudoku()
-    
-    return jsonify({"puzzle": puzzle})
+    difficulty = request.args.get('difficulty')
+    solution = generate_sudoku()
+    puzzle = create_puzzle(solution, difficulty)
+    return jsonify({"puzzle": puzzle, "solution": solution})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
